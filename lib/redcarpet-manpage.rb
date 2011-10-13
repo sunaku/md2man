ad5b746664bf938b1e6f6f6660e46fc4852483f4
Suraj N. Kurapati <sunaku@gmail.com>
2011-10-13 13:46:57 -0700

    bundler gem redcarpet-manpage

---
+++ lib/redcarpet-manpage.rb
@@ -0,0 +1,7 @@
+require "redcarpet-manpage/version"
+
+module Redcarpet
+  module Manpage
+    # Your code goes here...
+  end
+end
---
+++ lib/redcarpet-manpage/version.rb
@@ -0,0 +1,5 @@
+module Redcarpet
+  module Manpage
+    VERSION = "0.0.1"
+  end
+end
